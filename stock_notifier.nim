27e42f2edd9be295f1e29b164e11b9527e31b17f
Dominik Picheta <dominikpicheta@gmail.com>
2017-10-04 14:44:59 +0100

    Initial commit.

---
+++ stock_notifier.nim
@@ -0,0 +1,47 @@
+import asyncdispatch, httpclient, strutils, os, browsers, future
+
+import notifications/macosx
+
+const
+  argosUrl = "http://www.argos.co.uk/Product/7366077"
+  amazonUrl = "https://www.amazon.co.uk/Nintendo-Classic-Mini-" &
+              "Entertainment-System/dp/B073BVHY3F"
+
+proc onNotificationClick(info: ClickInfo, url: string) =
+  if info.kind == ClickKind.ActionButtonClicked:
+    openDefaultBrowser(url)
+
+proc checkArgosHasStock(): bool =
+  ## Returns true when Argos contains some stock.
+  echo("Checking argos")
+  let client = newHttpClient()
+  let content = client.getContent(argosUrl)
+
+  return "Currently unavailable" notin content
+
+proc checkAmazonHasStock(): bool =
+  ## Returns true when Amazon contains some stock.
+  echo("Checking amazon")
+  let client = newHttpClient("Mozilla/5.0 (Windows NT 10.0; Win64; x64) " &
+                             "AppleWebKit/537.36 (KHTML, like Gecko) " &
+                             "Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063")
+  let content = client.getContent(amazonUrl)
+
+  return "Available from" notin content
+
+while true:
+  echo("Checking stock...")
+  if checkArgosHasStock():
+    var center = newNotificationCenter(
+      (info: ClickInfo) => onNotificationClick(info, argosUrl)
+    )
+    waitFor center.show("In Stock", "Argos has stock!", actionButtonTitle="Open")
+
+  if checkAmazonHasStock():
+    var center = newNotificationCenter(
+      (info: ClickInfo) => onNotificationClick(info, amazonUrl)
+    )
+    waitFor center.show("In Stock", "Amazon has stock!",
+                        actionButtonTitle="Open")
+
+  sleep(60000*2)
---
+++ stock_notifier.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "Dominik Picheta"
+description   = "A little app for notifying me of SNES classic edition stock in Amazon/Argos."
+license       = "MIT"
+
+# Dependencies
+
+requires "nim >= 0.17.2"
+
+requires "notifications"
